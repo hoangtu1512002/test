cddaf202d4a097d6f7e0cef5c08ba0c6b8986089
Quoc2003kb <Buianhquoc07@gmail.com>
2024-11-12 11:14:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Tuyệt vời! 🤣";
+  question.innerHTML = "Dở math math 🤣";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
